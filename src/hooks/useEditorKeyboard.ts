2f8bc19ae5b99f75cc33aea9307607eea88df446
Ilkin Ibadov <ilkin.ibadov21@gmail.com>
2026-02-12 20:12:16 +0400

    bug fix

--- src/hooks/useEditorKeyboard.ts
+++ src/hooks/useEditorKeyboard.ts
@@ -1,14 +1,16 @@
 import { useMemo } from 'react'
 import type { KeyboardControlsEntry } from '@react-three/drei'
 
-export enum Controls {
-    forward = 'forward',    // ArrowUp 
-    back = 'back',          // ArrowDown
-    left = 'left',          // ArrowLeft
-    right = 'right',        // ArrowRight
-    delete = 'delete',      // Delete / Backspace
-    escape = 'escape',      // Escape key
-}
+export const Controls = {
+    forward: 'forward',    // ArrowUp 
+    back: 'back',          // ArrowDown
+    left: 'left',          // ArrowLeft
+    right: 'right',        // ArrowRight
+    delete: 'delete',      // Delete / Backspace
+    escape: 'escape',      // Escape key
+} as const;
+
+export type Controls = typeof Controls[keyof typeof Controls];
 
 export const useKeyboardMap = () =>
     useMemo<KeyboardControlsEntry<Controls>[]>(() => [
